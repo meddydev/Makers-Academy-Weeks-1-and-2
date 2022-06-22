34f9975fd929553a55a77c3cd0b2784b5c4151d1
Ahmed <ahmed@hallajian.co.uk>
2022-06-22 11:50:13 +0100

    Completed 'Crafting doubles' exercise and challenge

---
+++ Phase 3: Mocking Bites/Crafting doubles/spec/doubles_challenge_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe "doubles challenge" do
+    it "creates a sophisticated double" do
+        task = double(:fake_task)
+        task_list = double(:fake_list, count: 1, clear: :success, list: [task])
+        expect(task_list).to receive(:add).with(task)
+  
+      # Don't edit below
+        task_list.add(task)
+        expect(task_list.list).to eq [task]
+        expect(task_list.count).to eq 1
+        expect(task_list.clear).to eq :success
+    end
+end
